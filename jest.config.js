9188858a520f01e493cefa8fc89446abe8b0d6e3
ravipatirani <50319265+ravipatirani@users.noreply.github.com>
2021-04-16 19:22:04 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,9 @@
 module.exports = {
-  "roots": [
-    "<rootDir>/src"
-  ],
-  "testMatch": [
-    "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
-  ],
-  "transform": {
-    "^.+\\.(ts|tsx)$": "ts-jest"
-  },
-  "testResultsProcessor": "jest-sonar-reporter",
-  "collectCoverage": true,
-  "collectCoverageFrom": [
-    "**/*.{ts,js}",
-    "!**/node_modules/**"
-  ],
-  "coverageDirectory": "dist",
-  "coverageReporters": ["lcov"]
-}
+  reporters: [
+    'default',
+    [ 'jest-junit', {
+      outputDirectory: 'test_reports',
+      outputName: 'jest-junit.xml',
+    } ]
+  ]
+};
