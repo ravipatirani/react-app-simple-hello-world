7a8e2ff38866018ce6305c25dd3bddb73a8051ab
ravipatirani <50319265+ravipatirani@users.noreply.github.com>
2021-04-16 18:51:52 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,9 @@
 module.exports = {
-  projects: [
-    {
-      displayName: "Unit Tests",
-      testMatch: ["<rootDir>/__tests__/**/*.js"],
-      testPathIgnorePatterns: [
-        '<rootDir>/node_modules/',
-        '<rootDir>/__tests__/lib'
-      ],
-      testEnvironment: 'node',
-      setupFilesAfterEnv: ["<rootDir>/__tests__/lib/setupTests.js"]
-    },
-    "<rootDir>/integration-tests/testResultsProcessor/",
-    "<rootDir>/integration-tests/reporter/"
-  ],
-  reporters: ['default', '.']
+  reporters: [
+    'default',
+    [ 'jest-junit', {
+      outputDirectory: /,
+      outputName: JunitXml,
+    } ]
+  ]
 };
